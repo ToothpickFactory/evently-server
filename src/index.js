c678bee2b7fdc0d5d7c3ca422c63fbee2226efb9
Austin Lovell <alovell@goreact.com>
2017-09-11 15:20:46 -0600

    server does not turn on unless it has made a DB connections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-const config					= require("config");
-const express					= require("express");
+const config			= require("config");
+const express			= require("express");
 const app           	= express();
 const bodyParser	  	= require('body-parser');
 const cors          	= require('cors');
+const Mongo         	= require('./connections/mongo');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require('./api')(app);
-// require('./crons');
+Mongo.getDB()
+	.then(() => {
+		require('./api')(app);
+		require('./crons');
+		app.listen(config.port, () => { console.log(`Evently running on port: ${config.port}`) });
+	})
+	.catch(() => console.log('Server was unable to start'))
 
-app.listen(config.port, () => { console.log(`Sample App listing on port: ${config.port}`) });
+
